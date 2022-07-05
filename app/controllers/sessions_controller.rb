703d34ce0de738d897eda3fbfb0f2331d97cd63e
Cristobal Dominguez <cristobald@gmail.com>
2022-07-04 21:12:21 -0400

    Termina el trabajo Carlos

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'Logueado Correctamente'
+    else
+      flash.now[:notice] = 'Email o password inválida'
+      render action :new
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: 'Signed out successfully'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
   get 'cookies/set'
   get 'cookies/show'
   get 'cookies/delete'
 
+  get 'home/index'
+
+  resources :sessions, only: %i[new create destroy]
+  root 'home#index'
 end
